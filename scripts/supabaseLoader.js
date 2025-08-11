382e40f0a0d02177af9547ed78a52851bfdd7d72
Jordan Nezbeth <jordan.nezbeth1@gmail.com>
2025-08-11 08:36:57 -0400

    Update supabaseLoader.js

--- scripts/supabaseLoader.js
+++ scripts/supabaseLoader.js
@@ -3,7 +3,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://vqoirfycaqbaknfetdxj.supabase.co'
-const supabaseAnonKey = 'YOUR_REAL_ANON_KEY' // replace this line
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxb2lyZnljYXFiYWtuZmV0ZHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNzA5NDgsImV4cCI6MjA2Nzc0Njk0OH0.PJaoaaMQkQmBIdRQLPs7rYWzxivhQloOm2Gd6UTc204' // replace this line
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
